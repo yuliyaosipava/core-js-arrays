83062a79a3dacd07c5cdbf99a878f6cb5174d168
uto4ka101 <juliabrest3@gmail.com>
2025-01-09 10:39:39 +0300

    findAllOccurrences

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -83,8 +83,8 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.filter((element) => element === item).length;
 }
 
 /**
